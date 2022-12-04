3842ac45c6a747151815a1a6cab693eae21ae098
Jitty-04 <jittysjohn1997@gmail.com>
2022-12-04 07:42:55 +0530

    search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddComponent } from './add/add.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddComponent
+    AddComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
